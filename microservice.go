f719effba2abe8f1ddec3daf535cab55c143896b
Max Proske <mhcproske@gmail.com>
2018-06-10 00:21:20 -0700

    Set port using env variables, Add GET functionality

--- microservice.go
+++ microservice.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello, world!")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0] // Extract first message parameter
+	fmt.Fprintf(w, message)
 }
